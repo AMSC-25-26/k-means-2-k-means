e0aed99c6621645c85e6a8192ac2be1e983dac96
Emilio Dalla Torre <info@emiliodallatorre.it>
2025-12-06 12:11:08 +0100

    Added mock classifier

---
+++ k-means/KMeansClassifier.cpp
@@ -0,0 +1,10 @@
+//
+// Created by emiliodallatorre on 06/12/25.
+//
+
+#include "KMeansClassifier.h"
+
+vector<int> KMeansClassifier::fit() {
+    labels.resize(data.size(), 0);
+    return labels;
+}
---
+++ k-means/KMeansClassifier.h
@@ -0,0 +1,30 @@
+#ifndef K_MEANS_KMEANSCLASSIFIER_H
+#define K_MEANS_KMEANSCLASSIFIER_H
+
+#include <vector>
+
+
+using namespace std;
+
+class KMeansClassifier {
+public:
+    const int cluster_count;
+    const int max_iterations;
+
+    vector<vector<double> > data;
+    vector<int> labels;
+
+    // constructor that also accepts initial data
+    KMeansClassifier(const vector<vector<double> > &init_data, const int clusters, const int max_iter = 300)
+        : cluster_count(clusters), max_iterations(max_iter), data(init_data) {
+    }
+
+    vector<int> fit();
+
+    int save_state(const char *filename);
+
+    int load_state(const char *filename);
+};
+
+
+#endif //K_MEANS_KMEANSCLASSIFIER_H
