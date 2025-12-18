4ac9d0ddaf65392babbcf9e08889b8f8a198d79c
sebastiano-perni <sebastiano.perni@mail.polimi.it>
2025-12-18 06:44:51 +0100

    Fixed variables not initialized in KMeansClassifier.hpp and removed unused methods.

--- k-means/src/KMeansClassifier.hpp
+++ k-means/src/KMeansClassifier.hpp
@@ -12,29 +12,23 @@ public:
     int cluster_count;
     const int max_iterations;
 
-    vector<vector<double> > data;
+    vector<vector<double>> data;
 
     // constructor that also accepts initial data
-    KMeansClassifier(const vector<vector<double> > &init_data, const int clusters, const int max_iter = 300)
-        : cluster_count(clusters), max_iterations(max_iter), data(init_data) {
-    }
+    KMeansClassifier(const vector<vector<double>>& init_data, const int clusters, const int max_iter = 300)
+        : cluster_count(clusters), max_iterations(max_iter), data(init_data){}
 
     vector<int> fit();
 
-    int save_state(const char *filename);
-
-    int load_state(const char *filename);
-
 private:
     // Hereafter the internal state of the classifier
-    vector<vector<vector<double> > > centroids_history_sequential{{}}; // history of centroids
-    vector<vector<vector<double> > > centroids_history_parallel{{}}; // history of centroids
+    vector<vector<vector<double>>> centroids_history_sequential{{}}; // history of centroids
+    vector<vector<vector<double>>> centroids_history_parallel{{}}; // history of centroids
     vector<double> local_centroids_history; //history of centroids compatible with mpi
     vector<double> new_local_centroids_history;
-    int size_lch; //size of the initial vector of centroids
+    int size_lch{}; //size of the initial vector of centroids
     vector<int> labels_parallel, labels_sequential; // labels assigned to data points
-
-    int iteration_count_sequential, iteration_count_parallel = 0;
+    int iteration_count_sequential = 0, iteration_count_parallel = 0;
 
     void set_initial_centroids();
 
